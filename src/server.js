91d412c79d3b0c40229ae7f1d498fea9f156824d
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-06-14 19:51:43 +0300

    small fix

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { swaggerDocs } from './middlewares/swaggerDocs.js';
-// import { UPLOAD_DIR } from './constants/index.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
 
@@ -16,7 +15,6 @@ export const setupServer = () => {
 
   app.use(cors());
   app.use(cookieParser());
-  // app.use('/uploads', express.static(UPLOAD_DIR));
   app.use('/api-docs', swaggerDocs());
   app.use(
     pino({
